45bc535f79e9b2bd79913ce72c7ab9ca3cb9f502
Ashraful Islam <ashrafunit7@gmail.com>
2021-10-21 01:15:21 +0600

    my first react native app

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Hello React Native!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <br />
+      <Button title="change text" onPress={() => setOutputText('Welcome to React Native champ!!!')}/>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
