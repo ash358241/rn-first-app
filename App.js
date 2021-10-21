3d14b43d568a5b6c7b7f09ec44300ac3c1c2b693
Ashraful Islam <ashrafunit7@gmail.com>
2021-10-21 22:46:43 +0600

    flexbox dive

--- App.js
+++ App.js
@@ -6,23 +6,45 @@ export default function App() {
   const [outputText, setOutputText] = useState('Hello React Native!')
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <br />
-      <TextInput placeholder='Goal mamma' style={{borderBottomColor: 'black', borderBottomWidth: 1, outline: 'none'}}/>
-      <br />
-      <Button title="change text" onPress={() => setOutputText('Welcome to React Native champ!!!')}/>
-      {/* <StatusBar style="auto" /> */}
+      <View style={styles.viewOne}>
+        <Text>1</Text>
+      </View>
+
+      <View style={styles.viewTwo}>
+        <Text>2</Text>
+      </View>
+
+      <View style={styles.viewThree}>
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
   },
+  viewOne: {
+    backgroundColor: 'red',
+    height: 100,
+    width: 100,
+    // flex: 1
+  },
+  viewTwo: {
+    backgroundColor: 'blue',
+    height: 100,
+    width: 100,
+    // flex: 3
+  },
+  viewThree: {
+    backgroundColor: 'green',
+    height: 100,
+    width: 100,
+  }
 });
 
 
