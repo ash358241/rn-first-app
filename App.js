cb0377aabda4049f8c0d0095f6e9a0a3df76ed4d
Ashraful Islam <ashrafunit7@gmail.com>
2021-10-21 22:28:07 +0600

    stling component (inline)

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Hello React Native!')
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
+      <br />
+      <TextInput placeholder='Goal mamma' style={{borderBottomColor: 'black', borderBottomWidth: 1, outline: 'none'}}/>
       <br />
       <Button title="change text" onPress={() => setOutputText('Welcome to React Native champ!!!')}/>
       {/* <StatusBar style="auto" /> */}
@@ -22,3 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
